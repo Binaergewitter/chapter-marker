71b9bb8bc4d6fa87de6bea8f42d5486d05cf5443
makefu <github@syntax-fehler.de>
2020-09-02 00:18:43 +0200

    default.nix: using pwd as src

--- default.nix
+++ default.nix
@@ -3,13 +3,7 @@
 stdenv.mkDerivation rec {
   name = "chapter-marker-${version}";
   version = "master";
-  src = fetchFromGitHub {
-    owner = "makefu";
-    repo = "chapter-marker";
-    rev = "bff1dba47d40b00d32dca95262d339484c2ae5fb";
-    sha256 = "01nl01kc4f3gync5xkjp0al752rilyyc4zvjjbv5hdvh38vgaml7";
-  };
-
+  src = ./.;
   buildInputs = [ makeWrapper ];
 
   installPhase =
