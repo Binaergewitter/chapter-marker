960bbba441b3b0298c69e91cbe1f38ef8a68437c
makefu <github@syntax-fehler.de>
2021-09-14 21:28:04 +0200

    bgt-replace-text: init

--- chapter_marker/bgt_replace_text.py
+++ chapter_marker/bgt_replace_text.py
@@ -2,7 +2,6 @@
 """ usage: bgt-get-titles [options] [SHOW]
 
 options:
-    --out=FILE          file to output the titles to (default is stdout)
     --apikey=KEY        The etherpad APIKEY
 
 requires either PAD_APIKEY to be set  or --apikey to be supplied
@@ -15,7 +14,7 @@ import urllib.request
 import os
 from docopt import docopt
 
-url = "https://etherpad.euer.krebsco.de/api/1.2.15/getText?apikey={}&padID={}"
+baseurl = "https://etherpad.euer.krebsco.de/api/1.2.15/{}?apikey={}&padID={}"
 
 
 def current_show():
@@ -29,7 +28,6 @@ def main():
     args = docopt(__doc__)
     apikey = os.environ.get("PAD_APIKEY", args['--apikey'])
     show = args["SHOW"]
-    out = args["--out"]
 
     if not apikey:
         print(__doc__)
@@ -37,24 +35,17 @@ def main():
     if not show:
         show = current_show()
     try:
-        ret = requests.get(url.format(apikey, show)).json()["data"]["text"]
+        print(f"Fetching show {show}")
+        text = requests.get(baseurl.format("getText",apikey, show)).json()["data"]["text"]
+        print(f"replacing <SENDUNGSNUMMER> with {show.upper()}")
+        text = text.replace("<SENDUNGSNUMMER>",show.upper())
+        print("updating shownotes")
+        ret = requests.post(baseurl.format("setText",apikey, show),data={"text":text})
     except:
         print("for url:" + url.format(apikey,show))
         print(f"response: {requests.get(url.format(apikey,show))}")
     header = True
 
-    titles = []
-    for line in ret.split("\n"):
-        if line == "---":
-            header = False
-        if not header and line.startswith("## "):
-            titles.append(line.replace("## ", "").strip())
-
-    if not out:
-        print("\n".join(titles))
-    else:
-        with open(out, "w+") as f:
-            print("\n".join(titles), file=f)
 
 
 if __name__ == "__main__":
